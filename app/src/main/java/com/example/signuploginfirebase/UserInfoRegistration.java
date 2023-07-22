8431008b516495869bd64e424032b315aa86a5c8
ksnts <santosjohnkyllem@gmail.com>
2023-07-23 00:19:55 +0800

    Removed saved foods button, Modified signup sickness option to be a dropdown. Added background image designs

--- app/src/main/java/com/example/signuploginfirebase/UserInfoRegistration.java
+++ app/src/main/java/com/example/signuploginfirebase/UserInfoRegistration.java
@@ -6,8 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
@@ -23,23 +26,36 @@ import com.google.firebase.database.ValueEventListener;
 
 public class UserInfoRegistration extends AppCompatActivity {
 
-    TextInputLayout regName,regEmail, regAge, regHeight, regWeight, regSickness;
+    TextInputLayout regName,regEmail, regAge, regHeight, regWeight;
     Button regBtn;
     FirebaseDatabase rootNode;
     DatabaseReference reference;
+    Spinner sicknessSpinner;
+    String [] diseaseOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info_registration);
 
+
         regName = findViewById(R.id.name);
         regEmail = findViewById(R.id.userEmail);
         regAge = findViewById(R.id.Age);
         regHeight = findViewById(R.id.height_in_cm);
         regWeight = findViewById(R.id.weight_in_kg);
-        regSickness = findViewById(R.id.user_sickness);
+        sicknessSpinner = findViewById(R.id.sickness_spinner);
         regBtn = findViewById(R.id.button_submit);
+
+        diseaseOptions = getResources().getStringArray(R.array.diseaseOptions);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                diseaseOptions
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sicknessSpinner.setAdapter(adapter);
 
         rootNode = FirebaseDatabase.getInstance();
         reference = rootNode.getReference("users");
@@ -52,13 +68,15 @@ public class UserInfoRegistration extends AppCompatActivity {
                 String age = regAge.getEditText().getText().toString();
                 String height = regHeight.getEditText().getText().toString();
                 String weight = regWeight.getEditText().getText().toString();
-                String sickness = regSickness.getEditText().getText().toString();
+                String sickness = sicknessSpinner.getSelectedItem().toString();
 
                 UserHelper helperClass = new UserHelper(userName, userEmail, age, height, weight, sickness);
 
                 checkDuplicateAndRegister(helperClass);
             }
         });
+
+
 
         ImageView BtnBack = findViewById(R.id.button_userDetails_to_signup);
 
